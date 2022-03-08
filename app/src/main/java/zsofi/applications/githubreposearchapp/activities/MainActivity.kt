bcde933fee20d515857ff4f2f72adaf2cdf3706c
zsofia_mandi <mandi.zsofia@gmail.com>
2022-03-08 18:56:04 +0100

    Setting progress Dialog
    Creating progress dialog and setting it to be shown while loading the repositories

--- app/src/main/java/zsofi/applications/githubreposearchapp/activities/MainActivity.kt
+++ app/src/main/java/zsofi/applications/githubreposearchapp/activities/MainActivity.kt
@@ -1,10 +1,12 @@
 package zsofi.applications.githubreposearchapp.activities
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -14,6 +16,7 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
+import zsofi.applications.githubreposearchapp.R
 import zsofi.applications.githubreposearchapp.adapters.RepositoryAdapter
 import zsofi.applications.githubreposearchapp.databinding.ActivityMainBinding
 import zsofi.applications.githubreposearchapp.models.RepositoryModel
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
     private var httpClient: HttpClient? = null
+    var customProgressDialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +47,7 @@ class MainActivity : AppCompatActivity() {
         binding?.ibSearch?.setOnClickListener {
             if (!binding?.etSearchField?.text.isNullOrEmpty()) {
                 val repoSearchParameters = binding?.etSearchField!!.text.toString()
+                showProgressDialog()
                 lifecycleScope.launch() {
                     val repositories = getRepositories(repoSearchParameters)
                     setupReposRecyclerView(repositories)
@@ -91,60 +96,102 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun getRepositories(query_details: String) : ArrayList<RepositoryModel> {
         val repositoryModelList : ArrayList<RepositoryModel> = ArrayList()
-        val res =
-            httpClient?.get<JsonObject>("https://api.github.com/search/repositories") {
-                parameter("q", "$query_details")
+
+        try {
+            val res =
+                httpClient?.get<JsonObject>("https://api.github.com/search/repositories") {
+                    parameter("q", "$query_details")
+                }
+            val items = res?.get("items")?.jsonArray ?: error("Unexpected input for items")
+
+            // Taking out the required information from the JsonObject
+            for (i in 0 until items.size) {
+                val id = i
+                var name = items[i].jsonObject["name"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                var description = items[i].jsonObject["description"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                var gitHubLink = items[i].jsonObject["html_url"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                val stars = items[i].jsonObject["stargazers_count"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                val forks = items[i].jsonObject["forks_count"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                var lastUpdate = items[i].jsonObject["pushed_at"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                var createDate = items[i].jsonObject["created_at"]?.toString()
+                    ?: error("Unexpected format in an 'item")
+                var ownerName = items[i].jsonObject["owner"]?.jsonObject?.get("login")?.toString()
+                    ?: error("Unexpected format in an 'item")
+                var ownerAvatar =
+                    items[i].jsonObject["owner"]?.jsonObject?.get("avatar_url")?.toString()
+                        ?: error("Unexpected format in an 'item")
+                var ownerGitHubLink =
+                    items[i].jsonObject["owner"]?.jsonObject?.get("html_url")?.toString()
+                        ?: error("Unexpected format in an 'item")
+
+
+                // Creating substrings, cutting off the "" marks, converting values to the right look
+                name = name.substring(1, name.length - 1)
+                description = description.substring(1, description.length - 1)
+                gitHubLink = gitHubLink.substring(1, gitHubLink.length - 1)
+                lastUpdate = lastUpdate.substring(1, 11)
+                createDate = createDate.substring(1, 11)
+                ownerName = ownerName.substring(1, ownerName.length - 1)
+                ownerAvatar = ownerAvatar.substring(1, ownerAvatar.length - 1)
+                ownerGitHubLink = ownerGitHubLink.substring(1, ownerGitHubLink.length - 1)
+
+
+                // Creating a repository instance from Repository Model
+                val repository = RepositoryModel(
+                    id, name, description, gitHubLink, stars, forks, lastUpdate,
+                    createDate, ownerName, ownerAvatar, ownerGitHubLink
+                )
+
+                // Adding the repository to the list of repositories
+                repositoryModelList.add(repository)
+
+                runOnUiThread {
+                    cancelProgressDialog()
+                }
             }
-        val items = res?.get("items")?.jsonArray ?: error("Unexpected input for items")
 
-        // Taking out the required information from the JsonObject
-        for(i in 0 until items.size){
-            val id = i
-            var name = items[i].jsonObject["name"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            var description = items[i].jsonObject["description"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            var gitHubLink = items[i].jsonObject["html_url"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            val stars = items[i].jsonObject["stargazers_count"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            val forks = items[i].jsonObject["forks_count"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            var lastUpdate = items[i].jsonObject["pushed_at"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            var createDate = items[i].jsonObject["created_at"]?.toString()
-                ?: error("Unexpected format in an 'item")
-            var ownerName = items[i].jsonObject["owner"]?.jsonObject?.get("login")?.toString()
-                ?: error("Unexpected format in an 'item")
-            var ownerAvatar = items[i].jsonObject["owner"]?.jsonObject?.get("avatar_url")?.toString()
-                ?: error("Unexpected format in an 'item")
-            var ownerGitHubLink = items[i].jsonObject["owner"]?.jsonObject?.get("html_url")?.toString()
-                ?: error("Unexpected format in an 'item")
-
-
-            // Creating substrings, cutting off the "" marks, converting values to the right look
-            name = name.substring(1, name.length - 1)
-            description = description.substring(1, description.length - 1)
-            gitHubLink = gitHubLink.substring(1, gitHubLink.length - 1)
-            lastUpdate = lastUpdate.substring(1, 11)
-            createDate = createDate.substring(1, 11)
-            ownerName = ownerName.substring(1, ownerName.length - 1)
-            ownerAvatar = ownerAvatar.substring(1, ownerAvatar.length - 1)
-            ownerGitHubLink = ownerGitHubLink.substring(1, ownerGitHubLink.length - 1)
-
-
-            // Creating a repository instance from Repository Model
-            val repository = RepositoryModel(
-                id, name, description, gitHubLink, stars, forks, lastUpdate,
-                createDate, ownerName, ownerAvatar, ownerGitHubLink
-            )
-
-            // Adding the repository to the list of repositories
-            repositoryModelList.add(repository)
+        }catch (e: Exception){
+            e.printStackTrace()
+            cancelProgressDialog()
+            showAlertDialog()
 
         }
 
         return repositoryModelList
+
+    }
+
+    // Show progress dialog while loading repositories
+    private fun showProgressDialog(){
+        customProgressDialog = Dialog(this@MainActivity)
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog?.setCanceledOnTouchOutside(false)
+        customProgressDialog?.show()
+    }
+
+    // Cancel the progress dialog
+    private fun cancelProgressDialog(){
+        if(customProgressDialog != null){
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
+    }
+
+    private fun showAlertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Alert")
+        builder.setMessage("Sorry! Something went wrong.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        builder.setPositiveButton("OK"){dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
 
     }
 
